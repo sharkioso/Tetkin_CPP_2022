81aad40947b625c221d72a49ca7cb423d5c8f13b
sharkioso <134219400+sharkioso@users.noreply.github.com>
2023-06-02 12:22:15 +0500

    HW-2-5-1

---
+++ HW-2-5-1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "RationalNumber.h"
+using namespace std;
+int main()
+{
+    RationalNumbers chek(5,15);
+    RationalNumbers test(10, 15);
+    chek - test;
+    chek.reduction();
+    chek.Print();
+    cout << (chek < test) << endl;
+}
+
